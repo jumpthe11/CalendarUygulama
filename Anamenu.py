4731036c4d34bbaad36b8402bc69220fe30b1ac0
jumpthe11 <jumpthe11@gmail.com>
2023-06-20 07:11:28 +0300

    sonsurum

--- Anamenu.py
+++ Anamenu.py
@@ -1,7 +1,8 @@
 import PySimpleGUI as sg
 import sys
 layout = [[sg.Text('Giriş ekranı:'), sg.Button('Giriş')],
-          [sg.Text('Kayıt ekranı:'), sg.Button('Kayıt')]
+          [sg.Text('Kayıt ekranı:'), sg.Button('Kayıt')],
+          [sg.Text('ÇIKIŞ'), sg.Button('ÇIK')]
           
           ]
 
@@ -10,7 +11,7 @@ window = sg.Window('Ana menü hoş geldiniz', layout)
 while True:
     event, values = window.read()
     print(event, values)
-    if event == sg.WIN_CLOSED:
+    if event == sg.WIN_CLOSED or event =='ÇIK':
         break
     if event == 'Giriş':
         execfile('login.py')
--- login.py
+++ login.py
@@ -3,7 +3,18 @@ import sqlite3
 from Register import *
 conn = sqlite3.connect('Database.db')
 c = conn.cursor()
-
+c.execute("""CREATE TABLE IF NOT EXISTS kullanici(
+    Ad text, 
+    Soyad text, 
+    Kullanıcı Adı text, 
+    Password text, 
+    TC Kimlik No text, 
+    Telefon text, 
+    Email text,
+    Adres text,
+    KullanıcıType text
+)
+""")
 
 layout = [[sg.Text('Kullanıcı Adı:'),sg.Input(key='Kullanıcı')],
           [sg.Text('Parola:'),  sg.Input(key='Password')],
@@ -17,18 +28,18 @@ for i in range(2):
 while True:
     event, values = window.read()
     print(event, values)
-    Datas[0]=values['Kullanıcı']
-    Datas[1]=values['Password']
+
     if event == sg.WIN_CLOSED or event == 'Çık':
         break
     if event == 'Giriş yap':
+        Datas[0]=values['Kullanıcı']
+        Datas[1]=values['Password']
         search = searchKullaniciad(Datas[0])
         if search == None:
-            print("Böyle kullanıcı yok")
+            sg.popup("Böyle kullanıcı yok")
             continue
         logindurum = searchLogin(Datas[0],Datas[1])
         if logindurum != None:
-            print("oldu")
             execfile('Takvim.py')
 
 
